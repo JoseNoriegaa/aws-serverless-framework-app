b0d9ea46d6bb6f05627c345af6cebfcdb88e828a
Jose Noriega <josenoriega723@gmail.com>
2024-05-29 18:33:17 -0700

    feat: user like

--- src/functions/like-user/handler.ts
+++ src/functions/like-user/handler.ts
@@ -1,8 +1,32 @@
 // External dependencies
+import { UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import type { SQSEvent } from "aws-lambda";
 
+// Internal dependencies
+import dynamodb from "../../lib/dynamodb";
+
+// Types & Interfaces
+interface IBody {
+  id: string;
+}
+
 const handler = async (event: SQSEvent) => {
-  console.log(event);
+  const body = JSON.parse(event.Records[0].body) as IBody;
+  const { id } = body;
+
+  console.log(`target user "${id}"`);
+  const command = new UpdateItemCommand({
+    TableName: 'usersTable',
+    Key: { pk: { S: id } },
+    UpdateExpression: 'ADD likes :num',
+    ExpressionAttributeValues: {
+      ':num': { N: '1' },
+    },
+    ReturnValues: 'ALL_NEW'
+  });
+
+  const response = await dynamodb.send(command);
+  console.log(response);
 }
 
 export default handler;
