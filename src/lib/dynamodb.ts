8fbf67ff0455b437967b4f9eae95cac1da41d1c8
Jose Noriega <josenoriega723@gmail.com>
2024-05-29 22:54:03 -0700

    chore: add unit test for lib utils

--- src/lib/dynamodb.ts
+++ src/lib/dynamodb.ts
@@ -1,13 +1,7 @@
 // External dependencies
 import { DynamoDBClient, type DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
 
-// Internal dependencies
-import { IS_OFFLINE } from "../constants";
-
-const params: DynamoDBClientConfig = Object.freeze(IS_OFFLINE ? {
-  region: 'localhost',
-  endpoint: 'http://0.0.0.0:8000'
-} : {});
+const params: DynamoDBClientConfig = Object.freeze({});
 
 const dynamodb = new DynamoDBClient(params);
 
